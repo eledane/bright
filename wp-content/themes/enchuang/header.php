6d546519bcb42009a0a27144888c332719e49c7b
Elyzium <guangzhiwoxin@gmail.com>
2017-03-10 16:59:21 +0800

    nav walker for pc header menu

--- wp-content/themes/enchuang/header.php
+++ wp-content/themes/enchuang/header.php
@@ -57,48 +57,17 @@
 				  <li><a id="trigger-overlay" href="#"><i class="fa fa-search"></i></a></li>
 				</ul><!-- /.second-nav -->
 				<nav class="menu">
-			    <ul>
-				<li><a href="index.html">Home</a></li>
-				<li class="active sub">
-				  <a href="#">Sample Pages</a>
-					<ul>
-						<li><a href="about.html">About Us</a></li>
-						<li><a href="about_2.html">About Us 2</a></li>
-						<li><a href="services.html">Services</a></li>	
-						<li><a href="services_2.html">Services 2</a></li>	
-						<li><a href="full-width.html">Full Width</a></li>
-						<li><a href="left-sidebar.html">Left Sidebar</a></li>	
-						<li><a href="right-sidebar.html">Right Sidebar</a></li>		
-						<li class='sub'>
-							<a href='#'>Multi-Level Drop-Down</a>
-							<ul>
-							  <li><a href='#'>Drop-Down Example</a></li>
-							  <li><a href='#'>Drop-Down Example</a></li>								
-							  <li class='last'><a href='#'>Drop-Down Example</a></li>
-							</ul>
-						</li>
-					</ul>
-				</li>
-				<li class="active sub">
-				  <a href="portfolio_3_cols.html">Portfolio</a>
-					<ul>
-						<li><a href="portfolio_2_cols.html">2 Columns</a></li>
-						<li><a href="portfolio_3_cols.html">3 Columns</a></li>
-						<li><a href="portfolio_4_cols.html">4 Columns</a></li>
-						<li><a href="single_project.html">Single Project</a></li>
-					</ul>
-				</li>
-				<li class="active sub">
-				  <a href="#">Blog</a>
-					<ul>
-						<li><a href="blog.html">Right Sidebar</a></li>
-						<li><a href="blog_left_sidebar.html">Left Sidebar</a></li>
-						<li><a href="blog_full_width.html">Full Width</a></li>
-						<li><a href="blog_single_post.html">Single Post</a></li>	
-					</ul>
-				</li>
-				<li class="last"><a href="#" data-toggle="modal" data-target="#myModal">Contact Us</a></li>				
-			    </ul>
+          <ul>
+			       <?php
+               $header_menu_args = array(
+                      'theme_location' => 'header-menu',
+                      'container'      => false,
+                      'items_wrap'     => '%3$s',
+                      'walker'         =>  new Enchuang_Header_Menu()
+                      );
+                wp_nav_menu($header_menu_args);
+                ?>
+           </ul>
 				</nav>
 		      </div>
 		    </div>
